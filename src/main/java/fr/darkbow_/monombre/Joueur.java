418b6ab52495536a064eeea9ce7d40e2570aa11b
DarkBoow <maximeolivier77@gmail.com>
2020-12-20 11:40:21 +0100

    yo

--- src/main/java/fr/darkbow_/monombre/Joueur.java
+++ src/main/java/fr/darkbow_/monombre/Joueur.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class Joueur {
     private MonOmbre main;
-    int test = 0;
 
     public Joueur(MonOmbre monombre){this.main = monombre;}
     private List<Point> playerpoints;
