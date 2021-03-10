bd88298c6585a5c03faa9efc2e0816212da1ac21
BuildTools <unconfigured@null.spigotmc.org>
2021-03-10 21:48:42 +0100

    Animals Arrow

--- src/main/java/fr/darkbow_/animalsarrow/AnimalsArrow.java
+++ src/main/java/fr/darkbow_/animalsarrow/AnimalsArrow.java
@@ -1,15 +1,14 @@
 package fr.darkbow_.animalsarrow;
 
 import fr.darkbow_.animalsarrow.scoreboard.ScoreboardSign;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SpawnEggMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.scoreboard.Scoreboard;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -18,6 +17,8 @@ public class AnimalsArrow extends JavaPlugin {
     private AnimalsArrow instance;
 
     private Map<Player, ScoreboardSign> boards;
+
+    private Map<Arrow, Entity> customprojectiles;
 
     public AnimalsArrow getInstance() {
         return this.instance;
@@ -28,6 +29,7 @@ public class AnimalsArrow extends JavaPlugin {
         instance = this;
 
         this.boards = new HashMap<>();
+        this.customprojectiles = new HashMap<>();
 
         getServer().getPluginManager().registerEvents(new AnimalsArrowEvent(this), this);
 
@@ -45,6 +47,63 @@ public class AnimalsArrow extends JavaPlugin {
         return this.boards;
     }
 
+    public void  throwItem(Player player, ItemStack it, Entity e) {
+        Entity projectile = null;
+        Egg egg = null;
+        Snowball snowball = null;
+/*        Bukkit.broadcastMessage("§e§lType Item : §b" + it.getType().toString());*/
+        if(it.getType().toString().endsWith("SPAWN_EGG")) {
+/*            Bukkit.broadcastMessage("§6Egg Meta : §a" + it.getType().toString().replace("_SPAWN_EGG", ""));*/
+            projectile = e.getWorld().spawnEntity(e.getLocation(), Objects.requireNonNull(EntityType.fromName(it.getType().toString().replace("_SPAWN_EGG", ""))));
+/*            if(((SpawnEggMeta) it.getItemMeta()).getSpawnedType() == null) {
+                projectile = e.getWorld().dropItem(e.getLocation(), it);
+            } else {
+                projectile = e.getWorld().spawnEntity(e.getLocation(), EntityType.fromName(it.getType().toString().replace("SPAWN_EGG", "")));
+            }*/
+        } else if(it.getType() == Material.EGG || it.getType() == Material.SNOWBALL) {
+            if(it.getType() == Material.EGG) {
+                /*egg = player.launchProjectile(Egg.class);*/
+                egg = (Egg) e.getWorld().spawnEntity(e.getLocation(), EntityType.EGG);
+            } else if(it.getType() == Material.SNOWBALL) {
+                /*snowball = player.launchProjectile(Snowball.class);*/
+                snowball = (Snowball) e.getWorld().spawnEntity(e.getLocation(), EntityType.SNOWBALL);
+            }
+        } else if(it.getType().toString().contains("BOAT")) {
+            Boat boat = (Boat) e.getWorld().spawnEntity(e.getLocation(), EntityType.BOAT);
+            TreeSpecies treespecies = TreeSpecies.GENERIC;
+            if(it.getType() == Material.ACACIA_BOAT){ treespecies = TreeSpecies.ACACIA; }
+            if(it.getType() == Material.BIRCH_BOAT){ treespecies = TreeSpecies.BIRCH; }
+            if(it.getType() == Material.DARK_OAK_BOAT){ treespecies = TreeSpecies.DARK_OAK; }
+            if(it.getType() == Material.JUNGLE_BOAT){ treespecies = TreeSpecies.JUNGLE; }
+            if(it.getType() == Material.SPRUCE_BOAT){ treespecies = TreeSpecies.REDWOOD; }
+            if(it.getType() == Material.OAK_BOAT){ treespecies = TreeSpecies.GENERIC; }
+            boat.setWoodType(treespecies);
+            projectile = boat;
+        } else {
+            projectile = e.getWorld().dropItem(e.getLocation(), it);
+        }
+
+
+        if(projectile != null ) {
+            projectile.setVelocity(e.getVelocity());
+            projectile.addScoreboardTag("NoFallDamage");
+            e.setPassenger(projectile);
+            ((LivingEntity) projectile).setCollidable(false);
+        }
+
+        if(egg != null) {
+            egg.setVelocity(e.getVelocity());
+            e.setPassenger(egg);
+            ((LivingEntity) egg).setCollidable(false);
+        }
+
+        if(snowball != null) {
+            snowball.setVelocity(e.getVelocity());
+            ((LivingEntity) snowball).setCollidable(false);
+            e.setPassenger(snowball);
+        }
+    }
+
     public String bool(boolean b){
         String bool = null;
         if(b){
@@ -54,5 +113,9 @@ public class AnimalsArrow extends JavaPlugin {
         }
 
         return bool;
+    }
+
+    public Map<Arrow, Entity> getCustomprojectiles() {
+        return customprojectiles;
     }
 }
--- src/main/java/fr/darkbow_/animalsarrow/AnimalsArrowEvent.java
+++ src/main/java/fr/darkbow_/animalsarrow/AnimalsArrowEvent.java
@@ -1,10 +1,15 @@
 package fr.darkbow_.animalsarrow;
 
-import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 public class AnimalsArrowEvent implements Listener {
     private AnimalsArrow main;
@@ -12,12 +17,59 @@ public class AnimalsArrowEvent implements Listener {
     public AnimalsArrowEvent(AnimalsArrow vaguesdemonstres){this.main = vaguesdemonstres;}
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event){
-        Player player = event.getPlayer();
+    public void onArrowShoot(EntityShootBowEvent event){
+        if(event.getEntity() instanceof Player){
+            Player player = (Player) event.getEntity();
+
+            org.bukkit.inventory.meta.Damageable bowdurability = (org.bukkit.inventory.meta.Damageable) event.getBow().getItemMeta();
+            player.sendMessage("Durabilité : " + bowdurability.getDamage());
+            /*bowdurability.setDamage(bowdurability.getDamage()-1);
+            if(bowdurability.getDamage() == 0){
+                event.getBow().setAmount(event.getBow().getAmount()-1);
+                player.updateInventory();
+            } else if(bowdurability.getDamage() > 0){
+                event.getBow().setItemMeta((ItemMeta) bowdurability);
+            }*/
+
+            ItemStack otherhand = null;
+            if(event.getBow() != null){
+                if(event.getBow().isSimilar(player.getInventory().getItemInMainHand())){
+                    otherhand = player.getInventory().getItemInOffHand();
+                } else if(event.getBow().isSimilar(player.getInventory().getItemInOffHand())){
+                    otherhand = player.getInventory().getItemInMainHand();
+                }
+
+                if(otherhand != null && otherhand.getType() != Material.AIR){
+                    /*if(event.getProjectile() instanceof Arrow){
+                        ((Arrow) event.getProjectile()).setBounce(false);
+                    }*/
+
+                    /*event.setCancelled(true);*/
+                    main.throwItem(player, otherhand, event.getProjectile());
+
+                    player.updateInventory();
+                    Vector vector = player.getLocation().getDirection();
+                }
+            }
+        }
     }
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent event){
-        Player player = event.getPlayer();
+    public void onEntityDamage(EntityDamageEvent event){
+        if(event.getEntity().getScoreboardTags().contains("NoFallDamage")){
+            event.setCancelled(true);
+            Bukkit.broadcastMessage("Cause : " + event.getCause().name());
+            if(event.getCause() == EntityDamageEvent.DamageCause.FALL){
+                Bukkit.broadcastMessage("yooo");
+                event.getEntity().removeScoreboardTag("NoFallDamage");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event){
+        if(event.getEntity().getScoreboardTags().contains("NoFallDamage")){
+            event.setCancelled(true);
+        }
     }
 }
